45232e62a0b09fe893e147293edb0852fab9d607
crazyvillage <mag@crazyvillage.net>
2024-02-22 04:55:28 -0800

    ref: move file

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.51.0"
-    }
-  }
-}
-
 provider "google" {
   project = "inventor-414623"
   region  = "us-west1"
